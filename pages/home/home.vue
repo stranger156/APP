3449209a2791d42b8cdc5fff50efcf1c54e6fed5
EDGnobody <17763099645@163.com>
2025-03-14 17:07:41 +0800

    home完善2

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="userLayout">
+	<view class="userLayout pageBg">
 		<view class="userInfo">
 			<view class="avatar">
 				<image src="/static/wechat_icon.png" mode="aspectFit"></image>
@@ -11,19 +11,70 @@
 			<view class="list">
 				<view class="row">
 					<view class="left">
-						<uni-icons type="contact" size="30"></uni-icons>
+						<uni-icons type="checkbox-filled" size="30" color="#002FA7"></uni-icons>
 						<view class="text">我的学员</view>
 					</view>
 					<view class="right">
 						<view class="text">33</view>
-						<uni-icons type="right" size="30"></uni-icons>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="contact-filled" size="30" color="#002FA7"></uni-icons>
+						<view class="text">用户中心</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="info-filled" size="30" color="#002FA7"></uni-icons>
+						<view class="text">课程统计</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
 					</view>
 				</view>
 			</view>
 		</view>
 		
 		<view class="section">
-			
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="chat-filled" size="30" color="#002FA7"></uni-icons>
+						<view class="text">用户反馈</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="info-filled" size="30" color="#002FA7"></uni-icons>
+						<view class="text">关于我们</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="minus-filled" size="30" color="#002FA7"></uni-icons>
+						<view class="text">退出登录</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="forward" size="26" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -53,6 +104,41 @@
 		.nickname{
 			font-size: 40rpx;
 			padding: 20rpx 0 5rpx;
+		}
+	}
+	.section{
+		width: 690rpx;
+		margin: 50rpx auto;
+		border: 1px solid #eee;
+		border-radius: 10rpx;
+		box-shadow: 0 0 30rpx rgba(0,0,0,0.05);
+		.list{
+			.row{
+				display: flex;
+				justify-content: space-between;
+				padding: 0 30rpx;
+				height: 100rpx;
+				border-bottom: 1px solid #eee;
+				&:last-child{border-bottom: 0;}
+				.left{
+					display: flex;
+					align-items: center;
+					.text{
+						font-size: 35rpx;
+						padding-left: 15rpx;
+						color: #666;
+					}
+				}
+				.right{
+					display: flex;
+					align-items: center;
+					.text{
+						font-size: 30rpx;
+						padding-left: 15rpx;
+						color: #aaa;
+					}
+				}
+			}
 		}
 	}
 }	
