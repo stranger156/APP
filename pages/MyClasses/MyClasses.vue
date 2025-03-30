e0ef44703bb2724b80b957e3bb66d20b1d905e09
EDGnobody <17763099645@163.com>
2025-03-30 14:11:32 +0800

    班级界面后端链接

--- pages/MyClasses/MyClasses.vue
+++ pages/MyClasses/MyClasses.vue
@@ -1,38 +1,80 @@
 <template>
-	<view class="MyClasses">
-			<uni-list class="list">
-				<uni-list-item class="listItem" v-for="(item, index) in List1" 
-				showArrow :key="item.id" :to="`/pages/MyStudents/MyStudents`">
-					<template v-slot:body>
-						<view class="slot-box">
-							<text class="slot-text">{{item.name}}</text>
-						</view>
-					</template>
-				</uni-list-item>
-			</uni-list>
-	</view>
+    <view class="MyClasses">
+        <view v-if="loading">加载中...</view>
+        <view v-else-if="List1.length === 0">
+            <text>暂无数据</text>
+            <button @click="fetchClasses">重新加载</button>
+        </view>
+        <view v-else>
+            <uni-list class="list">
+                <uni-list-item class="listItem" v-for="(item, index) in List1" showArrow
+                :key="item" clickable @click="selectFaculty(item)">
+                    <template v-slot:body>
+                        <view class="slot-box">
+                            <text class="slot-text">{{item}}</text> <!-- 显示学院名称 -->
+                        </view>
+                    </template>
+                </uni-list-item>
+            </uni-list>
+        </view>
+    </view>
 </template>
 
 <script setup>
-import{ref}from"vue"
-let List1 = ref([
-	{id:1, name:"软件2201班"},
-	{id:2, name:"软件2202班"},
-	{id:3, name:"软件2203班"},
-	{id:4, name:"软件2204班"},
-	{id:5, name:"软件2205班"},
-])
+import { ref, onMounted } from "vue";
+import { getFaculties } from "../../utils/api.js"; // 导入 API 方法
+
+let List1 = ref([]);
+let loading = ref(true);
+
+// 获取学院数据
+const fetchClasses = () => {
+    getFaculties().then(res => {
+        if (res.code === 200) {
+			console.log(res);
+            List1.value = res.faculties;  // 将获取到的学院列表赋值给 List1
+            loading.value = false; // 停止加载动画
+        } else {
+            uni.showToast({
+                title: '获取数据失败',
+                icon: 'none'
+            });
+            loading.value = false; // 停止加载动画
+        }
+    }).catch(err => {
+        console.error("请求失败:", err);
+        uni.showToast({
+            title: '请求失败，请稍后重试',
+            icon: 'none'
+        });
+        loading.value = false; // 停止加载动画
+    });
+};
+
+// 选择学院并跳转到班级信息页面
+const selectFaculty = (faculty) => {
+    console.log("参数是",faculty);
+    // 跳转到 MyStudents 页面，并传递学院信息
+    uni.navigateTo({
+        url: '/pages/MyStudents/MyStudents?faculty='+faculty,
+    });
+};
+
+// 页面加载时自动调用 fetchClasses 获取数据
+onMounted(() => {
+    fetchClasses();
+});
 </script>
 
 <style lang="scss" scoped>
-.MyClasses{
-	.list{
-		.listItem{
-			height: 110rpx;
-			.slot-text{
-				font-size: 33rpx;
-			}
-		}
-	}
+.MyClasses {
+    .list {
+        .listItem {
+            height: 110rpx;
+            .slot-text {
+                font-size: 33rpx;
+            }
+        }
+    }
 }
 </style>
--- utils/api.js
+++ utils/api.js
@@ -18,29 +18,70 @@ export const register = (params) => {
     });
 };
 
-//获取学院信息
-export const getFaculties= () => {
-	const token = getTokenFromLocalStorage();
-    return request({
-        url: "/getFaculties",
-        method: 'GET',
-		headers:{  'Content-Type': 'application/x-www-form-urlencoded',
-		                'Authorization': `Bearer ${token}`
-		            }
-    });
+// 获取学院列表
+export const getFaculties = async () => {
+    try {
+        // 使用 await 等待 getTokenFromLocalStorage 获取 token
+        const token = await getTokenFromLocalStorage();  
+
+        if (!token) {
+            uni.showToast({
+                title: '请先登录',
+                icon: 'none'
+            });
+            return Promise.reject(new Error('Token 不存在'));
+        }
+
+        // 使用获取的 token 发送请求
+        return request({
+            url: "/getFaculties",
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    } catch (err) {
+        console.error('获取 Token 失败:', err);
+        uni.showToast({
+            title: '获取 Token 失败',
+            icon: 'none'
+        });
+        return Promise.reject(err);
+    }
 };
 
 //获取学院班级信息
-export const getClassesByFaculty = (params) => {
-	const token = getTokenFromLocalStorage();
-    return request({
-        url: "/getClassesByFaculty",
-        method: 'POST',
-		headers:{  'Content-Type': 'application/x-www-form-urlencoded',
-		              'Authorization': `Bearer ${token}`
-		            },
-		  data:params
-    });
+export const getClassesByFaculty = async (params) => {
+	try {
+	    // 使用 await 等待 getTokenFromLocalStorage 获取 token
+	    const token = await getTokenFromLocalStorage();  
+	
+	    if (!token) {
+	        uni.showToast({
+	            title: '请先登录',
+	            icon: 'none'
+	        });
+	        return Promise.reject(new Error('Token 不存在'));
+	    }
+	
+	    // 使用获取的 token 发送请求
+	    return request({
+	        url: "/getClassesByFaculty",
+	        method: 'POST',
+	    	headers:{  'Content-Type': 'application/x-www-form-urlencoded',
+	    	              'Authorization': `Bearer ${token}`
+	    	            },
+	    	data:params
+	    });
+	} catch (err) {
+	    console.error('获取 Token 失败:', err);
+	    uni.showToast({
+	        title: '获取 Token 失败',
+	        icon: 'none'
+	    });
+	    return Promise.reject(err);
+	}
 };
 //获取用户信息
 export const getUserInfo= () => {
